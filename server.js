a9dd98cbcb89b4f6211d4ec0aca55acff526ed41
Hojoon Kim <hojoon0724@gmail.com>
2023-12-19 14:54:47 -0800

    magic 8 ball express app created

--- server.js
+++ server.js
@@ -35,6 +35,38 @@ app.get("/tip/:total/:tip", (req, res) => {
   res.send(`The ${total} with a ${tipPercent}% tip is ${addedTotal}`);
 });
 
+app.get("/magic/:question", (req, res) => {
+  const eightBallResponseText = [
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",
+    "As I see it yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful",
+  ];
+  const randomIndexNumber = Math.round(Math.random() * eightBallResponseText.length);
+  const questionText = req.params.question;
+  res.send(`
+  ${questionText}?<br>
+  <h1>
+  ${eightBallResponseText[randomIndexNumber]}
+  </h1>`);
+});
+
 // -----------------------------------------------------
 // Listener
 // -----------------------------------------------------
